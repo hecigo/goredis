3f7b85fa405f50a479bf00511adcbb115c6db102
Fioenix <phuong.tang@hoang-phuc.net>
2022-11-24 14:56:53 +0700

    A lot of update 😂

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package goredis
 
 import (
 	"context"
+	"time"
 
 	"errors"
 	"reflect"
@@ -14,15 +15,16 @@ import (
 type ctxKeyType_Redis string
 
 const (
-	HASH                               = "hash"
-	LIST                               = "list"
-	SET                                = "set"
-	ZSET                               = "zset" // sorted set
-	STRING                             = "string"
-	Stream                             = "stream"
-	CtxKey_DataType   ctxKeyType_Redis = "redis_data_type"
-	CtxKey_SliceStart ctxKeyType_Redis = "redis_slice_start"
-	CtxKey_SliceStop  ctxKeyType_Redis = "redis_slice_stop"
+	HASH                                 = "hash"
+	LIST                                 = "list"
+	SET                                  = "set"
+	ZSET                                 = "zset" // sorted-set
+	STRING                               = "string"
+	Stream                               = "stream"
+	CtxKey_DataType     ctxKeyType_Redis = "redis_data_type"
+	CtxKey_SliceStart   ctxKeyType_Redis = "redis_slice_start"
+	CtxKey_SliceStop    ctxKeyType_Redis = "redis_slice_stop"
+	CtxKey_SliceReverse ctxKeyType_Redis = "redis_slice_rev"
 )
 
 // Get one or multiple values by key(s). If the key does not exist, the value will be nil.
@@ -32,23 +34,34 @@ const (
 //
 //  1. [T]: the type of the value
 //
-//     - T is string, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, bool, time.Time, time.Duration or struct,
-//     it will get from Redis [STRING] key and convert to the given type.
+//     - As default, it will get value from Redis [STRING] key.
 //
-//     - T is a map[string]any, it always get from Redis [HASH]. Because of Redis limitation, if T is a map of struct, recommended to use non-nested struct.
+//     - T is a map[string]interface{} and [ctx] has [CtxKey_DataType] = [HASH], it will get value from Redis [HASH] key.
+//     Because of Redis limitation, if T is a map of struct, recommended to use only non-nested struct.
 //
-//     - T is a slice, it will get from Redis [STRING] as default;
-//     from Redis [LIST] if [ctx] contains key [CtxKey_RedisDataType] with value [LIST]; or
-//     from Redis [SET] if [ctx] contains key [CtxKey_RedisDataType] with value [SET].
+//     - T is a slice, and [ctx] has [CtxKey_DataType] = [LIST], it will get value from Redis [LIST] key;
+//     [ctx] has [CtxKey_DataType] = [SET], it will get value from Redis [SET] key;
+//     [ctx] has [CtxKey_DataType] = [ZSET], it will get value from Redis [ZSET] key (sorted-set).
 //
-//  2. [ctx]: includes [goutils.CtxConnNameKey] to specify the connection name. It also used to get the key prefix and track APM.
+//  2. [ctx]:
+//
+//     - Including [goutils.CtxConnNameKey] to specify the connection name. It also used to get the key prefix and track APM.
+//
+//     - Including [CtxKey_DataType] to specify the data type of the Redis key.
 //
 //  3. [keys]: the key without prefix.
 //
 // # Notes:
 //
-//  1. This function would return all elements in a Redis [LIST] or [SET] as default, so it is not recommended to use it for a long list/set.
-//  2. Allowing get a range from Redis [LIST] or [ZSET] (sorted set) by setting [ctx] with key [CtxKey_SliceStart] and [CtxKey_SliceStop].
+//  1. Should not use this function to get a long list/set. It will cause performance issue.
+//
+//  2. Allowing get a range from Redis [LIST] or [ZSET] (sorted-set) by setting [ctx] with values:
+//
+//     - [CtxKey_SliceStart]: the start index of the range
+//
+//     - [CtxKey_SliceStop]: the stop index of the range
+//
+//     - [CtxKey_SliceReverse]: true (default) to order the sorted-set by descending of score
 func Get[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	if len(keys) == 0 {
 		return nil, errors.New("keys is empty")
@@ -64,9 +77,14 @@ func Get[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	case reflect.String:
 		return getString(ctx, keys...)
 
-	// map[string]interface{}
-	case reflect.Map:
-		return getHash[T](ctx, keys...)
+	// map[string]any or struct
+	case reflect.Map | reflect.Struct:
+		switch ctx.Value(CtxKey_DataType) {
+		case HASH:
+			return getHash[T](ctx, keys...)
+		default:
+			return getVariousKind[T](ctx, keys...)
+		}
 
 	// slice
 	case reflect.Slice:
@@ -81,17 +99,78 @@ func Get[T any](ctx context.Context, keys ...string) (interface{}, error) {
 			return getVariousKind[T](ctx, keys...)
 		}
 
-	case reflect.Struct:
+	// time.Time, time.Duration, struct and various kinds of int, float, bool...
+	default:
+		return getVariousKind[T](ctx, keys...)
+	}
+}
+
+// Set value(s) to a unique key. If the key already exists, it will be overwritten.
+//
+// # Parameters:
+//
+//  1. [value]:
+//
+//     - As default, it will set value to Redis [STRING] key.
+//
+//     - If value is a map[string]interface{} or struct and [ctx] has [CtxKey_DataType] = [HASH], it will set to Redis [HASH].
+//     Because of Redis limitation, if value is a map of struct, recommended to use non-nested struct.
+//
+//     - If value is a slice, and [ctx] has [CtxKey_DataType] = [LIST], it will set to Redis [LIST];
+//     [ctx] has [CtxKey_DataType] = [SET], it will set to Redis [SET];
+//
+//  2. expiration: time-to-live of the key. If it is 0 or omitted, the key will never expire.
+//
+// # Notes:
+//
+//  1. This function does not support to set value to Redis [ZSET] because [ZSET] is a special data type.
+//
+//  2. This function will delete the old key first, then set the new list/set, sothat should not use it to set a long list/set.
+func SetSingle(ctx context.Context, key string, value interface{}, expiration ...time.Duration) error {
+	if key == "" {
+		return errors.New("key is empty")
+	}
+	if value == nil {
+		return errors.New("value is nil")
+	}
+
+	var expi time.Duration = 0 // never expire
+	if len(expiration) > 1 {
+		expi = expiration[0]
+	}
+
+	// get type of value
+	tType := reflect.TypeOf(value)
+	tKind := tType.Kind()
+
+	switch tKind {
+	// string
+	case reflect.String:
+		return setVariousKind(ctx, key, value, expi)
+
+	// map[string]any or struct
+	case reflect.Map | reflect.Struct:
 		switch ctx.Value(CtxKey_DataType) {
 		case HASH:
-			return getHash[T](ctx, keys...)
+			return setHash(ctx, key, value, expi)
 		default:
-			return getVariousKind[T](ctx, keys...)
+			return setVariousKind(ctx, key, value, expi)
+		}
+
+	// slice
+	case reflect.Slice:
+		switch ctx.Value(CtxKey_DataType) {
+		case SET:
+			return setSet(ctx, key, value, expi)
+		case LIST:
+			return setList(ctx, key, value, expi)
+		default:
+			return setVariousKind(ctx, key, value, expi)
 		}
 
 	// time.Time, time.Duration, struct and various kinds of int, float, bool...
 	default:
-		return getVariousKind[T](ctx, keys...)
+		return setVariousKind(ctx, key, value, expi)
 	}
 }
 
@@ -183,13 +262,30 @@ func getVariousKind[T any](ctx context.Context, keys ...string) (interface{}, er
 	}
 }
 
+// Set string(s) to Redis.
+func setVariousKind(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	status, err := Client(ctx).Set(ctx, addKeyPrefix(ctx, key)[0], value, expiration).Result()
+	if err != nil {
+		return err
+	}
+	if status != "OK" {
+		goutils.Errorf("setVariousKind: status is %s\n", status)
+		goutils.Errorf("key is %s\n", key)
+		goutils.Errorf(" value is %v\n", value)
+		return errors.New("redis set status is not OK")
+	}
+	return nil
+}
+
 // Get hash from Redis.
 func getHash[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	if len(keys) == 0 {
 		return nil, errors.New("key is empty")
 	}
 
-	tIsTruct := ctx.Value(CtxKey_DataType) == HASH
+	// T is struct
+	var t T
+	tIsTruct := reflect.TypeOf(t).Kind() == reflect.Struct
 
 	// get single key-value
 	if len(keys) == 1 {
@@ -251,6 +347,45 @@ func getHash[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	return goutils.Unmarshal[map[string]*T](r)
 }
 
+// Set hash to Redis. The value must be a struct or a map.
+func setHash(ctx context.Context, key string, value interface{}, expiration time.Duration) (err error) {
+
+	// convert value to map[string]interface{}
+	var temp map[string]interface{}
+	switch value := value.(type) {
+	case map[string]interface{}:
+		temp = value
+	default:
+		temp, err = goutils.Unmarshal[map[string]interface{}](value)
+		if err != nil {
+			return err
+		}
+	}
+
+	// convert temp to slice of interface{}
+	var val []interface{}
+	for k, v := range temp {
+		val = append(val, k, v)
+	}
+
+	// set key-value
+	key = addKeyPrefix(ctx, key)[0]
+	status, err := Client(ctx).HMSet(ctx, key, val...).Result()
+	if err != nil {
+		return err
+	}
+	if !status {
+		goutils.Errorf("setHash: status is %v\n", status)
+		goutils.Errorf("key is %s\n", key)
+		goutils.Errorf(" value is %v\n", value)
+		return errors.New("redis set status is not OK")
+	}
+
+	// set expiration
+	err = setExpiration(ctx, key, expiration)
+	return err
+}
+
 // Get all elements of list from Redis.
 func getList[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	if len(keys) == 0 {
@@ -290,6 +425,29 @@ func getList[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	return redisCmdToMap[T](ctx, reflect.TypeOf(t).Elem(), keys, cmds, err)
 }
 
+// Set list to Redis. The value must be a slice.
+// This action will delete the old list and set a new one.
+func setList(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	val := value.([]interface{})
+	key = addKeyPrefix(ctx, key)[0]
+
+	// delete old list
+	_, err := Client(ctx).Del(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+
+	// set new list
+	_, err = Client(ctx).RPush(ctx, key, val...).Result()
+	if err != nil {
+		return err
+	}
+
+	// set expiration
+	err = setExpiration(ctx, key, expiration)
+	return err
+}
+
 // Get set from Redis.
 func getSet[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	if len(keys) == 0 {
@@ -315,7 +473,30 @@ func getSet[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	return redisCmdToMap[T](ctx, reflect.TypeOf(t).Elem(), keys, cmds, err)
 }
 
-// Get sorted set from Redis.
+// Set set to Redis. The value must be a slice.
+// This action will delete the old set and set a new one.
+func setSet(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	val := value.([]interface{})
+	key = addKeyPrefix(ctx, key)[0]
+
+	// delete old set
+	_, err := Client(ctx).Del(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+
+	// set new set
+	_, err = Client(ctx).SAdd(ctx, key, val...).Result()
+	if err != nil {
+		return err
+	}
+
+	// set expiration
+	err = setExpiration(ctx, key, expiration)
+	return err
+}
+
+// Get sorted-set from Redis.
 func getSortedSet[T any](ctx context.Context, keys ...string) (interface{}, error) {
 	if len(keys) == 0 {
 		return nil, errors.New("key is empty")
@@ -325,9 +506,10 @@ func getSortedSet[T any](ctx context.Context, keys ...string) (interface{}, erro
 		t     T
 		start int64 = 0
 		stop  int64 = -1
+		rev         = true
 	)
 
-	// get sorted set start and stop from context
+	// get sorted-set start and stop from context
 	i := ctx.Value(CtxKey_SliceStart)
 	if i != nil {
 		start = i.(int64)
@@ -336,17 +518,31 @@ func getSortedSet[T any](ctx context.Context, keys ...string) (interface{}, erro
 	if i != nil {
 		stop = i.(int64)
 	}
+	i = ctx.Value(CtxKey_SliceReverse)
+	if i != nil {
+		rev = i.(bool)
+	}
 
-	// get single key-value
+	// get single key-value with ZRangeArgs
 	if len(keys) == 1 {
-		cmd := Client(ctx).ZRange(ctx, addKeyPrefix(ctx, keys...)[0], start, stop)
+		cmd := Client(ctx).ZRangeArgs(ctx, redis.ZRangeArgs{
+			Key:   addKeyPrefix(ctx, keys...)[0],
+			Start: start,
+			Stop:  stop,
+			Rev:   rev,
+		})
 		return redisCmdToSlice[T](ctx, reflect.TypeOf(t).Elem(), cmd)
 	}
 
 	// get multiple key-values
 	cmds, err := Client(ctx).TxPipelined(ctx, func(pipe redis.Pipeliner) error {
 		for _, k := range addKeyPrefix(ctx, keys...) {
-			pipe.ZRange(ctx, k, start, stop)
+			pipe.ZRangeArgs(ctx, redis.ZRangeArgs{
+				Key:   k,
+				Start: start,
+				Stop:  stop,
+				Rev:   rev,
+			})
 		}
 		return nil
 	})
@@ -415,4 +611,23 @@ func redisCmdToMap[T any](ctx context.Context, eleType reflect.Type, keys []stri
 	}
 
 	return goutils.Unmarshal[map[string]*T](r)
+}
+
+// set expiration for key
+func setExpiration(ctx context.Context, key string, expiration time.Duration) error {
+	if expiration == 0 {
+		return nil
+	}
+
+	status, err := Client(ctx).Expire(ctx, key, expiration).Result()
+	if err != nil {
+		return err
+	}
+	if !status {
+		goutils.Errorf("setExpiration: status is %v\n", status)
+		goutils.Errorf("key is %s\n", key)
+		return errors.New("redis set status is not OK")
+	}
+
+	return nil
 }
